4420fc2a38f0c4763ec592bee6a9594b44352c40
Kuleb1n <v_vpyt@mail.ru>
2023-02-02 20:10:31 +0300

    class NewsAPIView(generics.ListCreateAPIView)

--- news/views.py
+++ news/views.py
@@ -1,31 +1,8 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 from .models import News
 from .serializers import NewsSerializer
 
 
-class NewsAPIView(APIView):
-
-    def get(self, request):
-        news = News.objects.all()
-        return Response({'post': NewsSerializer(news, many=True).data})
-
-    def post(self, request):
-        serializer = NewsSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'new_news': serializer.data})
-
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({'Error': 'Method PUT not allowed.'})
-        try:
-            instance = News.objects.get(pk=pk)
-        except:
-            return Response({'Error': 'Object does not exists.'})
-
-        serializer = NewsSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'updated news': serializer.data})
+class NewsAPIView(generics.ListCreateAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
