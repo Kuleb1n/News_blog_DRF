1d70a70256d2785eca2e9b920bc01767d905a028
Kuleb1n <v_vpyt@mail.ru>
2023-02-03 16:10:04 +0300

    routes 'api/v1/news/<int:pk>/' and 'api/v1/news-detail/<int:pk>/'

--- news/urls.py
+++ news/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [path('api/v1/news', NewsAPIView.as_view()),
-               path('api/v1/news/<int:pk>/', NewsAPIView.as_view()),
+               path('api/v1/news/<int:pk>/', NewsUpdateAPIView.as_view()),
+               path('api/v1/news-detail/<int:pk>/', NewsDetailAPIView.as_view()),
                ]
