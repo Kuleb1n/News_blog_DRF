0c2be9c912fceecb332543fa4206ae5608a8e051
Kuleb1n <v_vpyt@mail.ru>
2023-02-03 16:08:49 +0300

    adding a class NewsUpdateAPIView and NewsDetailAPIView

--- news/views.py
+++ news/views.py
@@ -6,3 +6,13 @@ from .serializers import NewsSerializer
 class NewsAPIView(generics.ListCreateAPIView):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
+
+
+class NewsUpdateAPIView(generics.UpdateAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+
+
+class NewsDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
