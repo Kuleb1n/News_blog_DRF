db951f590e2dfcaa4ea709751ac5f5464397147e
Kuleb1n <v_vpyt@mail.ru>
2023-02-04 19:55:47 +0300

    adding output of all categories and categories by pk

--- news/views.py
+++ news/views.py
@@ -1,18 +1,24 @@
-from rest_framework import generics
-from .models import News
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from .models import News, Category
 from .serializers import NewsSerializer
 
 
-class NewsAPIView(generics.ListCreateAPIView):
+class NewsViewSet(viewsets.ModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
 
+    @action(methods=['get'], detail=False)
+    def categories(self, request):
+        category = Category.objects.all()
+        return Response({'categories': [category.category_name for category in category]})
 
-class NewsUpdateAPIView(generics.UpdateAPIView):
-    queryset = News.objects.all()
-    serializer_class = NewsSerializer
-
-
-class NewsDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = News.objects.all()
-    serializer_class = NewsSerializer
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk):
+        try:
+            category = Category.objects.get(pk=pk)
+            return Response({'category': category.category_name})
+        except:
+            return Response({'Error': f'There is no category with such a key: {pk}'})
