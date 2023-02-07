bd1766934ae0bc8286a76889618377e421e8b793
Kuleb1n <v_vpyt@mail.ru>
2023-02-07 21:22:41 +0300

    adding permission_classes

--- news/views.py
+++ news/views.py
@@ -3,12 +3,14 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import News, Category
+from .permissions import IsUserOrReadOnly
 from .serializers import NewsSerializer
 
 
 class NewsViewSet(viewsets.ModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
+    permission_classes = (IsUserOrReadOnly, )
 
     @action(methods=['get'], detail=False)
     def categories(self, request):
