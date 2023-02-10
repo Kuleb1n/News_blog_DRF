85204c00bc1d055a457b785c3a95c357e2f2bdaf
Kuleb1n <v_vpyt@mail.ru>
2023-02-10 15:03:38 +0300

    adding pagination_class

--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 from .models import News, Category
@@ -7,10 +8,17 @@ from .permissions import IsUserOrReadOnly
 from .serializers import NewsSerializer
 
 
+class LargeResultsSetPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
+
 class NewsViewSet(viewsets.ModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
-    permission_classes = (IsUserOrReadOnly, )
+    permission_classes = (IsUserOrReadOnly,)
+    pagination_class = LargeResultsSetPagination
 
     @action(methods=['get'], detail=False)
     def categories(self, request):
