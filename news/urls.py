ce13287658752c9b247c36fedc3ec20c5332aa1a
Kuleb1n <v_vpyt@mail.ru>
2023-02-10 15:02:57 +0300

    adding paths for JWTAuthentication

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include, re_path
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from news.views import NewsViewSet
 
@@ -10,6 +11,9 @@ router.register(r'news', NewsViewSet, basename='news')
 urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('api/v1/drf-auth/', include('rest_framework.urls')),
-    path('api/v1/auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    # path('api/v1/auth/', include('djoser.urls')),
+    # re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
